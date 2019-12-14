e3cdfabb43778d5c69256dbbd48c40e411c9a834
Konstantin Rachev <konstantin@fundamental.bg>
2019-12-14 23:14:03 +0200

    Updated service provider

--- src/EpayServiceProvider.php
+++ src/EpayServiceProvider.php
@@ -14,7 +14,7 @@ class EpayServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/config/epay.php' => config_path('epay.php'),
+            __DIR__ . '/../config/epay.php' => config_path('epay.php'),
         ], 'config');
     }
     /**
